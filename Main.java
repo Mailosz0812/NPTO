faf18f86ac2eaa363b38478866bc541ad1ab8ed0
Mailosz0812 <89217@student.pb.edu.pl>
2024-04-08 09:32:16 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,10 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        for(int i = 0; i < 5; i++)
+        {
+            System.out.println("Hello World");
+        }
 
     }
 }
